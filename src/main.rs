97ab41e99c7bd7eec9d48b2f4e90cf3637b593f2
exploding-pear <alexjdupper@gmail.com>
2020-08-23 13:31:53 -0400

    automating testing boilerplate code

--- src/main.rs
+++ src/main.rs
@@ -42,19 +42,35 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
+}
+
+pub trait Testable {
+    fn run(&self);
+}
+
+impl<T> Testable for T
+where
+    T: Fn(), // T must implement the Fn() trait i.e. be a function
+{
+    fn run(&self) {
+        // printing the function name (for functions, the type is the function name)
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        // calling test function
+        self();
+        // printing ok
+        serial_println!("[ok]");
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
